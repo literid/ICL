8d52e6676449d2db67ab72456b3484cac2214008
nickolay <hohlovkola@gmail.com>
2024-01-07 16:16:48 +0300

    Updated MLP output axis order

--- src/models/MLP.py
+++ src/models/MLP.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 
-class MLP(nn.Module):
+class MetaLearningMLP(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim) -> None:
         super().__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
@@ -9,8 +9,13 @@ class MLP(nn.Module):
         self.fc2 = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
+        """
+        `B` - batch size, `N` - number of examples, `D` - `input_dim`, `C` - `output_dim`
+        Args: x: torch.Tensor shape `(B, N, D)`
+        Returns: torch.Tensor shape `(B, C, N)`
+        """
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
 
-        return x
+        return x.permute(0, 2, 1)
