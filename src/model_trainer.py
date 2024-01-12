887086d36fc268b3fb58ad26aec1dff9a28e33c3
nickolay <hohlovkola@gmail.com>
2024-01-12 14:11:59 +0300

    Add early stopping support

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -3,7 +3,14 @@ import torch
 
 class Trainer:
     def __init__(
-        self, model, optimizer, loss_fn, device, train_loader, val_loader=None
+        self,
+        model,
+        optimizer,
+        loss_fn,
+        device,
+        train_loader,
+        val_loader=None,
+        early_stopping_patience=None,
     ):
         self.model = model
         self.optimizer = optimizer
@@ -11,6 +18,12 @@ class Trainer:
         self.loss_fn = loss_fn
         self.train_loader = train_loader
         self.val_loader = val_loader
+
+        if early_stopping_patience is not None:
+            assert self.val_loader is not None, "Validation data not provided"
+            self.early_stopping = EarlyStopping(early_stopping_patience)
+        else:
+            self.early_stopping = None
 
     def train_epoch(self):
         self.model.train()
@@ -48,7 +61,10 @@ class Trainer:
         average_loss = total_loss / num_batches
         return average_loss
 
-    def train(self, num_epochs):
+    def train(
+        self,
+        num_epochs,
+    ):
         for epoch in range(num_epochs):
             train_loss = self.train_epoch()
             if self.val_loader is not None:
@@ -56,5 +72,26 @@ class Trainer:
                 print(
                     f"Epoch {epoch + 1}/{num_epochs} - Train Loss: {train_loss:.4f} - Validation Loss: {val_loss:.4f}"
                 )
+                if self.early_stopping is not None and self.early_stopping.should_stop(
+                    val_loss
+                ):
+                    print("Early stopping triggered")
+                    break
             else:
                 print(f"Epoch {epoch + 1}/{num_epochs} - Train Loss: {train_loss:.4f}")
+
+
+class EarlyStopping:
+    def __init__(self, patience=10, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.count = 0
+        self.min_val_loss = float("inf")
+
+    def should_stop(self, val_loss):
+        if val_loss < self.min_val_loss:
+            self.min_val_loss = val_loss
+            self.count = 0
+        else:
+            self.count += 1
+        return self.count >= self.patience
